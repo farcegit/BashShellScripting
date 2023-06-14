840feb59428d652cdbc845b548f8100656cb28cb
farcegit <136146492+farcegit@users.noreply.github.com>
2023-06-13 23:13:41 -0600

    Adding listing text
    Adding listing text One lines ifs

--- countlines.sh
+++ countlines.sh
@@ -1,16 +1,16 @@
 function checkOptions(){
 while getopts "om" opt; do
     case $opt in
-        o) listFilterFiles $opt $2;;
-        m) listFilterFiles $opt $2;;
+        o) listFilterFiles $opt $2 "owner";;
+        m) listFilterFiles $opt $2 "month";;
         \?) showHelp;;
     esac
  done
 }
 
 function listFilterFiles(){
-filenames=`ls ./`
-for filename in $filenames
+echo "Looking for files where the $3 is : $2"    
+for filename in `ls ./`
 do
    if ( [ $1 == "o" ] && [ `isOwner $filename $2` == 1 ] ) || ( [ $1 == "m" ] && [ `isMonth $filename $2` == 1 ] )
    then 
@@ -20,21 +20,11 @@ done
 }
 
 isOwner(){
-    if [ $2 == `stat --format "%U" $1` ]
-    then
-        echo 1
-    else
-        echo 0
-    fi
+    if [ $2 == `stat --format "%U" $1` ];then echo 1; else echo 0;fi
 }
 
 isMonth(){
-    if [ $2 == `ls -l $1 | awk -F " " '{print $6}'` ]
-    then
-        echo 1
-    else
-        echo 0
-    fi
+    if [ $2 == `ls -l $1 | awk -F " " '{print $6}'` ];then echo 1; else echo 0;fi
 }
 
 showHelp(){
